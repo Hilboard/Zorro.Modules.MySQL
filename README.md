6bc4e54a3c86dc9a3f91a40f57dc3dc881d1750a
artem <akhilya@yandex.ru>
2025-06-29 15:31:55 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Zorro.Modules.MySQL
+
+MySQL implementation for Zorro
+
+> [!CAUTION]
+> Project is currently under heavy production. Do not expect regular updates and stability. Use with caution.
---
+++ Services/MySQLService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using MySqlConnector;
+
+namespace Zorro.Services;
+
+public static class MySQLService
+{
+    public static MySqlDbContextOptionsBuilder GetDefaultOptions(MySqlDbContextOptionsBuilder builder)
+    {
+        var optionsBuilder = builder.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null
+        );
+        return optionsBuilder;
+    }
+
+    public delegate MySqlConnectionStringBuilder ConnectionStringBuilder(MySqlConnectionStringBuilder builder);
+    public static ConnectionStringBuilder? ConnectionStringMaster { get; set; } = null;
+
+    public static MySqlConnectionStringBuilder DefaultConnectionStringBuilder { get; set; } = new();
+
+    public static void UseMySQL(DbContextOptionsBuilder options)
+    {
+        UseMySQL(options, null, true);
+    }
+
+    public static void UseMySQL(
+        DbContextOptionsBuilder options,
+        Action<MySqlDbContextOptionsBuilder>? optionsBuilder,
+        bool useDefaultOptions
+    )
+    {
+        MySqlConnectionStringBuilder connectionStringBuilder = new(DefaultConnectionStringBuilder.ConnectionString);
+
+        if (ConnectionStringMaster is not null)
+            ConnectionStringMaster.Invoke(connectionStringBuilder);
+
+        string connectionString = connectionStringBuilder.ToString();
+
+        options.UseMySql(
+            connectionString,
+            ServerVersion.AutoDetect(connectionString),
+            mySQLOptions =>
+            {
+                if (useDefaultOptions)
+                    mySQLOptions = GetDefaultOptions(mySQLOptions);
+                optionsBuilder?.Invoke(mySQLOptions);
+            }
+        );
+    }
+
+}
